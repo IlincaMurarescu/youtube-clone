8129e974d23ae3eee35ce89c913f9a7c71eb6421
IlincaMurarescu <ilincamurarescu@gmail.com>
2024-04-05 00:01:09 +0300

    header + added styled

--- youtube-clone/src/components/Header.jsx
+++ youtube-clone/src/components/Header.jsx
@@ -1,24 +1,54 @@
 import styles from "./Header.module.css";
+import styled from "styled-components";
 
 export default function Header() {
   return (
-    <header>
-      <div className="container">
-        <button className="toggle button-round ">Toggle</button>
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <button className={styles.button}>
+          {" "}
+          <img
+            src="src\assets\toggle-column-svgrepo-com.svg"
+            className={styles.iconw}
+          ></img>
+        </button>
         <img className="logo  "></img>
       </div>
-      <div className="container ">
+      <div className={styles.formContainer}>
         <form>
           <input />
-          <button>Lens icon</button>
+          <button className={styles.formButton}>
+            <img
+              src="src\assets\magnifying-glass-svgrepo-com.svg"
+              className={styles.iconw}
+            ></img>
+          </button>
         </form>
-        <button>Microphone icon</button>
+        <button className={styles.formButton}>
+          {" "}
+          <img
+            src="src\assets\microphone-svgrepo-com.svg"
+            className={styles.iconw}
+          ></img>
+        </button>
       </div>
-      <div className="container">
-        <button>Camera</button>
-        <button>Notifications</button>
+      <div className={styles.container}>
+        <button className={styles.button}>
+          {" "}
+          <img src="src\assets\+ (3).svg" className={styles.iconh}></img>
+        </button>
+        <button className={styles.button}>
+          {" "}
+          <img
+            src="src\assets\notification-4-svgrepo-com.svg"
+            className={styles.iconh}
+          ></img>
+        </button>
 
-        <button>Avatar</button>
+        <button className={styles.avatarButton}>
+          {" "}
+          <img src="src\assets\+.png" className={styles.avatar}></img>
+        </button>
       </div>
     </header>
   );
